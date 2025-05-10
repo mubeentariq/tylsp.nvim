c2642b922b5c49b3162fd4ff4d1f4d0d408d15c6
Sitt Guruvanich <aekazitt@gmail.com>
2025-05-11 02:17:15 +0700

    feat: ensure plenary is installed before setup

--- plugin/tylsp.lua
+++ plugin/tylsp.lua
@@ -1,4 +1,6 @@
 -- plugin/tylsp.lua --
 
-if not pcall(require, 'lspconfig') then return end
+if not pcall(require, 'lspconfig') and not pcall(require, 'plenary') then
+  return
+end
 require('tylsp').setup()
